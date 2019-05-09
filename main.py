d6c818d2a134f6b843553937c2f1bc590845b0fb
ClockworkKettle <timothyjmcnamara@gmail.com>
2019-05-09 14:43:35 +0100

    Created Main Python file

---
+++ main.py
@@ -0,0 +1,9 @@
+# Take LAS file as input
+# Optional Clip to a Shapefile, KML file, GEOjson
+# Optional accept a KML that is a path and fill in Shapefile
+# Options ( Paramaeters ) -- Resolution
+# Output GeoRectified DSM || GeoRectified DTM
+
+
+def __init__():
+    print("Initialized")
